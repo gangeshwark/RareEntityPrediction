526eed791e2ebddf108a36c522c2490698af6e2b
zhanghao-T560 <isaac.changhau@gmail.com>
2018-01-26 16:19:04 +0800

    add dataset preprocess codes

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # RareEntityPrediction
+
+**You can Delete the following after you read it**
+
+**Note**: create a `data` folder in your home(`~`) directory and place the `rare_entity` folder into it.
+
+- **dataset folder**: used to process the raw data (`entities.txt` and `corpus.txt`, now I'm running the codes), which generate `all_data.json`, then load the json data, shuffle it and split it into three part *80%* for training, *10%* for dev and *10%* for test (codes are preparing).
+```json
+{
+  "sentence": "<sentence contains **BLANK** to be filled>",
+  "supplementary": "<supplementary sentence (maybe used)>",
+  "candidates": "<list of candidates>",
+  "descriptions": "<descriptions for each candidates>",
+  "answer": "<Answer candidate>"
+}
+```
+
+- **prepro folder**: used to read train/dev/test dataset and prepared vocabularies, embeddings, padded data and etc.
---
+++ dataset/build_dataset.py
@@ -0,0 +1,85 @@
+import os
+import re
+from tqdm import tqdm
+from nltk import sent_tokenize, word_tokenize
+import json
+import pickle
+from dataset.corenlp import CoreNLP
+
+corenlp = CoreNLP()  # not used
+
+home = os.path.expanduser('~')
+source_dir = os.path.join(home, 'data', 'rare_entity')
+
+prog = re.compile(r'(9202a8c04000641f8\w+)')
+id_replace = '**BLANK**'
+
+
+def prepro_entities(entity_file, dump_id_name=None, dump_name_desc=None):
+    id_name = {}
+    name_desc = {}
+    with open(entity_file, 'r', encoding='utf-8') as f:
+        for line in tqdm(f, desc='process entities'):
+            fb_id, *_, name, full_desc = line.strip().split('\t')
+            desc = sent_tokenize(full_desc, language='english')[0]
+            desc = ' '.join([token.replace("``", '"').replace("''", '"') for token in word_tokenize(desc)])
+            id_name[fb_id] = name
+            name_desc[name] = desc
+    if dump_id_name is not None:
+        with open(dump_id_name, 'w') as din:
+            pickle.dump(id_name, din)
+    if dump_name_desc is not None:
+        with open(dump_name_desc, 'w') as dnd:
+            pickle.dump(name_desc, dnd)
+    return id_name, name_desc
+
+
+def prepro_corpus(corpus_file, id_name, name_desc):
+    data = []
+    with open(corpus_file, 'r', encoding='utf-8') as f:
+        for line in tqdm(f, desc='process corpus'):  # loop each document
+            all_ids = prog.findall(line)
+            candidates = [id_name[fb_id] for fb_id in all_ids]
+            descriptions = [name_desc[name] for name in candidates]
+            sents = sent_tokenize(line, language='english')
+            sents = [sent.replace("``", '"').replace("''", '"') for sent in sents if len(sent) >= 10]
+            for i in range(len(sents)):  # process each sentence
+                if not prog.search(sents[i]):  # no freebase ids are contained
+                    continue
+                ids = prog.findall(sents[i])  # obtain all freebase ids in a sentence
+                for fb_id in ids:
+                    answer = id_name[fb_id]
+                    other_ids = [x for x in ids if x != fb_id]  # get other freebase ids
+                    fst_sent = sents[i].replace(fb_id, id_replace)  # replace target id with **BLANK**
+                    for sub_id in other_ids:
+                        fst_sent = fst_sent.replace(sub_id, id_name[sub_id])  # replace other ids with actual name
+                    # obtain the second sentence as a supplementary
+                    scd_sent = sents[i + 1] if i < len(sents) - 1 else sents[i - 1]
+                    sub_ids = prog.findall(scd_sent)
+                    for sub_id in sub_ids:
+                        scd_sent = scd_sent.replace(sub_id, id_name[sub_id])
+                    # store each record into dict
+                    record = {
+                                'sentence': fst_sent,
+                                'supplementary': scd_sent,
+                                'candidates': candidates,
+                                'descriptions': descriptions,
+                                'answer': answer
+                             }
+                    data.append(record)
+    return data
+
+
+def main():
+    entity_file = os.path.join(source_dir, 'entities.txt')
+    corpus_file = os.path.join(source_dir, 'corpus.txt')
+    id_name, name_desc = prepro_entities(entity_file, dump_id_name='id_name.pk', dump_name_desc='name_desc.pk')
+    print(len(id_name))
+    data = prepro_corpus(corpus_file, id_name, name_desc)
+    print(len(data))
+    with open('all_data.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+if __name__ == '__main__':
+    main()
